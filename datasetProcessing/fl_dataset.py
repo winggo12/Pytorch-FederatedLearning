Fixed Acc/Loss Calculation

datasetProcessing/fl_dataset.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import pandas as pd
+import math
 import numpy as np
 from sklearn.utils import shuffle
 import torch
@@ -29,6 +30,10 @@ class TheDataset(Dataset):
     def __init__(self, in_tensor, out_tensor):
         self.X_ = in_tensor
         self.Y_ = out_tensor
+
+    def get_dataset_size(self):
+        return self.X_.shape[0]
+
     def __len__(self):
         return len(self.Y_)
     def __getitem__(self , idx):
@@ -43,7 +48,8 @@ class DatasetSplitByDirichletPartition():
             raise NameError("Ratio is not in correct range")
         df = pd.read_csv(file_path)
         df_new = df.sort_values(by=['CreditLevel'])
-
+        row_num, col_num = df.shape[1], df.shape[0]
+        self.__dataset_size = col_num
         #Get the number of labels and create a dictionary
         #that stores such info
         #e.g. {0: 100, 1:200}
@@ -54,6 +60,7 @@ class DatasetSplitByDirichletPartition():
             label_num_dict[label] = count
 
         label_id_dict = {}
+        label_sizes = []
         count = 0
 
         #Get the id of all data and store them
@@ -65,13 +72,31 @@ class DatasetSplitByDirichletPartition():
                 id_list.append(count)
                 count += 1
             label_id_dict[k] = id_list
+            label_sizes.append(len(id_list))
+
+        #Find the minimum size of data of a
+        #certain label
+        min_size = min(label_sizes)
+        min_partition = 1 / min_size
 
         #Using dirichlet distribution to distribute
         #the data to users and put them into dict
         #label_id_dict_of_users : eg {0: {0:[1],1:[4]},1: {0: [2,3],1:[5,6]}}
-        partitions = np.random.dirichlet(np.repeat(alpha, user_num))
-        print("------Data Distribution for Users------")
-        print(partitions)
+        #Keeps getting distribution until each partition is not too small
+        status = [ False ]
+        while False in status:
+            status = []
+            partitions = np.random.dirichlet(np.repeat(alpha, user_num))
+            # print(partitions)
+            for partition in partitions:
+                if partition < min_partition:
+                    status.append(False)
+                else:
+                    status.append(True)
+
+        self.__partitions = partitions
+        # print("------Data Distribution for Users------")
+        # print(partitions)
         label_id_dict_of_users = {}
         label_id_dict_of_user = {}
         user_id = 0
@@ -121,6 +146,12 @@ class DatasetSplitByDirichletPartition():
 
     def get_dataset_dict(self):
         return self.dataloader_dict
+
+    def get_partitions(self):
+        return self.__partitions
+
+    def get_complete_dataset_size(self):
+        return self.__dataset_size
 
 if __name__ == '__main__':
     file_path = '../data/BankChurners_normalized_standardized.csv'

fl_train.py
@@ -18,22 +18,34 @@ def average_weights(w):
     """
     w_avg = copy.deepcopy(w[0])
     for key in w_avg.keys():
-        for i in range(1, len(w)):
+        for i in range(0, len(w)):
             w_avg[key] += w[i][key]
         w_avg[key] = torch.div(w_avg[key].float(), len(w))
     return w_avg
 
-if __name__ == '__main__':
-    alpha = 100
+def weighted_average_weights(w, partitions):
+    w_avg = copy.deepcopy(w[0])
+    for key in w_avg.keys():
+        for i in range(1, len(w)):
+            w_avg[key] += torch.mul(w[i][key], partitions[i])
+        w_avg[key] = torch.div(w_avg[key].float(), len(w))
+    return w_avg
+
+def train(log = True):
+    alpha = 1000
     user_num = 5
-    global_rounds = 20
-    local_epochs = 10
-    batch_size = 32
+    global_rounds = 10
+    local_epochs = 5
+    batch_size = 4
+    avg_global_test_acc , avg_global_test_loss = 0 , 0
+
     spliter = DatasetSplitByDirichletPartition(file_path=config.data_path,
                                                alpha=alpha,
                                                user_num=user_num,
                                                train_ratio=.8)
     dataset_dict = spliter.get_dataset_dict()
+    partitions = spliter.get_partitions()
+    dataset_size = spliter.get_complete_dataset_size()
 
     global_model = FcNet()
 
@@ -43,19 +55,45 @@ if __name__ == '__main__':
         global_acc = []
 
         for user_index in range(user_num):
+            local_dataset_size = dataset_dict[user_index]['train'].X_.shape[0]
+            if log == True:
+                print("_________________Local Trainer w/ size: ", local_dataset_size, "/",
+                      dataset_size, "_________________________")
             model_weights, loss = local_trainer(dataset=dataset_dict[user_index]['train'],
                                                 model=copy.deepcopy(global_model),
                                                 global_round=round_idx,
                                                 local_epoch=local_epochs,
-                                                batch_size = batch_size)
+                                                batch_size = batch_size,
+                                                log = log)
             local_weights.append(copy.deepcopy(model_weights))
             local_losses.append(loss)
 
-        global_weight = average_weights(local_weights)
+        # global_weight = average_weights(local_weights)
+        global_weight = weighted_average_weights(local_weights, partitions)
         global_model.load_state_dict(global_weight)
+
         test_acc, test_loss = inference(dataset=dataset_dict[user_index]['test'],
                                         model=global_model,
-                                        batch_size=batch_size)
-        print("_________________Global Trainer_________________________")
-        print('Global Round :{}, the global accuracy is {:.3}%, and the global loss is {:.3}.'.format(round_idx, 100 * test_acc, test_loss))
-        print("__________________________________________")
+                                        batch_size=batch_size,
+                                        log = log)
+
+        avg_global_test_acc += (test_acc*100)
+        avg_global_test_loss += test_loss
+        if log == True:
+            print("_________________Global Trainer_________________________")
+            print('Global Round :{}, the global accuracy is {:.3}%, and the global loss is {:.3}.'.format(round_idx, 100 * test_acc, test_loss))
+            print("________________________________________________________")
+
+    avg_global_test_acc , avg_global_test_loss =  avg_global_test_acc/global_rounds, avg_global_test_loss/global_rounds
+
+    print("_________________Final Result_________________________")
+    print("Partitions = ", partitions)
+    print("Alpha = ", alpha, " User Number = ", user_num)
+    print('Final global accuracy is {:.3}%, and the Final global loss is {:.3}.'.format(test_acc*100,test_loss))
+    print('Average global accuracy is {:.3}%, and the Average global loss is {:.3}.'.format(avg_global_test_acc,avg_global_test_loss))
+    print("________________________________________________________")
+
+if __name__ == '__main__':
+    for i in range(10):
+        print("Trial Number {:}".format(i))
+        train(log=False)

trainer/trainer.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-def local_trainer(dataset, model, global_round, local_epoch, batch_size):
+def local_trainer(dataset, model, global_round, local_epoch, batch_size, log=True):
     iteration = 0
     running_corrects, running_corrects_per_itr = 0 , 0
     running_loss, running_loss_per_itr = 0 , 0
@@ -10,7 +10,9 @@ def local_trainer(dataset, model, global_round, local_epoch, batch_size):
     loss_func = nn.CrossEntropyLoss()
     model.train()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    print("_________________Local Trainer_________________________")
+    dataset_size = dataset.X_.shape[0]
+    required_iterations = int( (dataset_size/batch_size)*local_epoch )
+
     for epoch in range(local_epoch):
         for inputs, labels in dataloader:
             optimizer.zero_grad()
@@ -27,19 +29,20 @@ def local_trainer(dataset, model, global_round, local_epoch, batch_size):
             running_corrects += torch.sum(preds.data == ground_truth.data)
             running_corrects_per_itr += torch.sum(preds.data == ground_truth.data)
 
-            if iteration % 100 == 0 and iteration!=0:
-                acc = running_corrects_per_itr / (100 * batch_size)
-                loss = running_loss_per_itr / (100 * batch_size)
+            if iteration  == required_iterations and iteration!=0:
+                acc = running_corrects_per_itr / (required_iterations * batch_size)
+                loss = running_loss_per_itr / (required_iterations * batch_size)
                 running_corrects_per_itr = 0
                 running_loss_per_itr = 0
-                print('Stage: Train  Global Round {} Iteration: {} Acc: {}  Loss: {}'.format(global_round,iteration,acc,loss))
+                if log == True:
+                    print('Stage: Train  Global Round {} Iteration: {} Acc: {}  Loss: {}'.format(global_round,iteration,acc,loss))
             iteration += 1
 
     final_loss = running_loss/(iteration)
 
     return model.state_dict(), final_loss
 
-def inference(dataset, model, batch_size):
+def inference(dataset, model, batch_size, log=True):
     iteration = 0
     running_corrects, running_corrects_per_itr = 0, 0
     running_loss, running_loss_per_itr = 0, 0
@@ -63,8 +66,8 @@ def inference(dataset, model, batch_size):
 
         iteration += 1
 
-    final_acc = running_corrects/(iteration*batch_size)
-    final_loss = running_loss / (iteration*batch_size)
+    final_acc = running_corrects/(dataset.get_dataset_size())
+    final_loss = running_loss / (dataset.get_dataset_size())
 
 
     return final_acc, final_loss